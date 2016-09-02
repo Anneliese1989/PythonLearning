2d8b7d01d72548b907c04cb03f22ad33ed4deabf
David <yiry098@hotmail.com>
2016-09-02 16:39:17 +0800

    Update Pythonlearn.py

--- Pythonlearn.py
+++ Pythonlearn.py
@@ -1,5 +1,4 @@
 # created date 2016-8-30
-
 import io
 
 import mysql.connector
@@ -9,30 +8,27 @@ def connect():
     #""" Connect to MySQL database """
     try:
         conn = mysql.connector.connect(host='localhost',
-                                       database='testschema',
+                                       database='world',
                                        user='root',
                                        password='********')
         if conn.is_connected():
-            print('Connected to MySQL database')
+            print('Connected to MySQL database\n')
             curosor=conn.cursor()
-            query = ("select * from student")
+            query = ("SELECT Code,Name,Continent,Region FROM world.country where LifeExpectancy>70")
             #hire_start = datetime.date(1999, 1, 1)
             #hire_end = datetime.date(1999, 12, 31)
             curosor.execute(query)
-            list=[]
-            for (stu_id,stu_name,stu_guid,stu_Address) in curosor:
-                list.append([stu_id,stu_name,stu_guid,stu_Address])
-
-            for (stu_id,stu_name,stu_guid,stu_Address) in list:
-                print stu_id,stu_name,stu_guid,stu_Address
-     
-         
-    except Error as e:
+            
+            for Code,Name,Continent,Region in curosor:
+                print Code,Name,Continent,Region
+    
+    except errorcode as e:
         print(e)
  
     finally:
         conn.close()
  
- 
 if __name__ == '__main__':
     connect()
+ 
+  
