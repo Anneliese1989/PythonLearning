8b30a5f9af2581e7b872ad9e09850ade05643cd5
David <yiry098@hotmail.com>
2016-09-05 17:21:23 +0800

    Update webConnection.py

--- webConnection.py
+++ webConnection.py
@@ -4,11 +4,34 @@
 
 import urllib
 
-from urllib.request import urlopen
-try:
-	with	urlopen("http://stackoverflow.com/questions/5685406/inconsistent-use-of-tabs-and-spaces-in-indentation", timeout=2)	as	present:	
-		for target_list in present:
-			print(target_list)	
-	print("working connection") 
-except Exception as err:
-	print(err)
+from urllib.request import *
+
+ 
+class ConItnt:
+	i=6
+	data=[]
+
+	def	__init__(self):		#invoked as instantiation
+		data=[1,2,3,4,5]
+		for i in data:
+			print(i,"initialed as instantiation")
+
+	def func(self):
+		print("this is a function.")
+
+
+	def conn(self):
+		try:
+			with urlopen("http://stackoverflow.com/questions/5685406/inconsistent-use-of-tabs-and-spaces-in-indentation",timeout=10) as target:
+				for t in target:
+					print(t)
+			
+		except expression as identifier:
+			print(identifier)
+
+
+
+
+if __name__=='__main__':
+	x=ConItnt()
+	x.conn()
